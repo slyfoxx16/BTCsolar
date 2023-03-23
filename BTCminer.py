d0f51b3a49fb6f973253f602bd15e5bd471e7c88
slyfoxx16 <carxtega16@gmail.com>
2023-03-22 23:55:57 -0700

    update

--- BTCminer.py
+++ BTCminer.py
@@ -1,21 +1,49 @@
-"""
-BTCminer
+# BTC Miner
 
-Created By Slyfox
-
-Custom class BTCblock that contains high level info of a BTC block
-BTCblock class has function to pull furhter detail about block
-
-Function getBTCdata creates list of BTCblocks based on start and end time
-
-URL Reference:
-https://www.blockchain.com/api/blockchain_api
-"""
-
-import BTCdata
+# Packages
+import pandas as pd
+import random
+import SolarSimulator
 
 class BTCminer():
-	def __init__(self,Herts,Watts,var):
-		self.Herts = Herts
-		self.kW = Watts
-		self.var = var
+	def __init__(self,Th,W):
+		self.Th = Th
+		self.kW = W/1000
+		self.yearly_kWh = 8760 * (W/1000)
+
+	def fit_solar(self, pv_system, nrel_api_key, nrel_api_email, n_iter):
+		# Enter Location and specs of pv system
+		soalr_sys = SolarSimulator.SolarSimulator(pv_system, nrel_api_key, nrel_api_email)
+
+		# define the bounds for the inputs and the number of iterations
+		# x_bounds and y_bounds should be tuples with (min, max) values
+		esitmated_capacity_factor = 0.2
+		starting_cap = int(self.yearly_kWh/(esitmated_capacity_factor * 8760))
+
+		best_params = None
+		best_distance = float('inf')
+		
+		# Bounds for search
+		min_cap = starting_cap - 5
+		max_cap = starting_cap + 5
+		
+		for i in range(n_iter):
+			# generate random values for the parameters within the given bounds
+			ac = random.randint(min_cap, max_cap)
+			dc = random.randint(min_cap, max_cap)
+			
+			# evaluate the function with the current parameters
+			output = sum(soalr_sys.simulate_solar(ac, dc)['shape'].solar)/1000
+			
+			# compute the distance between the output and the target value
+			distance = abs(output - self.yearly_kWh)
+			
+			# update the best parameters and distance if the current output is closer to the target
+			if output >= self.yearly_kWh and distance < best_distance:
+				best_params = (ac, dc)
+				best_distance = distance
+
+		print(f"Optimal Solar Capacity (kW): ac={best_params[0]}:dc={best_params[1]}")
+		print(f"Output with best parameters: {sum(soalr_sys.simulate_solar(*best_params)['shape'].solar)/1000}")
+		
+		return best_params
--- SolarSimulator.py
+++ SolarSimulator.py
@@ -9,76 +9,71 @@
 import pandas as pd
 import PySAM.Pvwattsv8 as pvwatt8
 import PySAM.ResourceTools as tools
-from functools import reduce
-import itertools
 
+# Define a class named SolarSimulator
 class SolarSimulator():
-	def __init__(self, system_info):
-		# create new instance of pvwatt8
-		self.pvwatt = pvwatt8.new()
-		# assign system information passed to constructor
-		self.system_info = system_info
-		# set system specs and fetch weather data
-		self.set_system_specs()
-		self.fetch_weather_data()
+    """Class to simulate a solar pv system. 
+    Requires NREL API key and registered email"""
 
-	def set_system_specs(self):
-		# assign system information specs to system_design attribute of pvwatt
-		self.pvwatt.SystemDesign.assign(self.system_info['specs'])
-		# assign adjustment factor dict to adjustment_factors attribute  of pvwatt
-		self.pvwatt.AdjustmentFactors.assign({'constant':0})
+    def __init__(self, system_info, nrel_api_key, nrel_api_email):
+        # create an instance of pvwatt8
+        self.pvwatt = pvwatt8.new()
+        # assign system information passed to constructor
+        self.system_info = system_info
+        # set system specs and fetch weather data
+        self.set_system_specs()
+        self.fetch_weather_data(nrel_api_key, nrel_api_email)
 
-	def fetch_weather_data(self):
-		# get latitude and longitude from system info to fetch weather data
-		location = [(self.system_info['location']['lon'], self.system_info['location']['lat'])]
-		# create instance of FetchResourceFiles using nrel api
-		nrel = tools.FetchResourceFiles(
-			tech='solar',
-			nrel_api_key='bfpWE6iufkhWFk9DfN1G7M4FYeUIlWNp3MCOaQ2Z',
-			nrel_api_email='carxtega16@gmail.com'
-		)
-		# fetch resource files based on location
-		nsrdb_path_dict = nrel.fetch(location).resource_file_paths_dict
-		# assign fetched solar resource data path to solar_resource_file attribute of pvwatt
-		self.pvwatt.SolarResource.assign({'solar_resource_file':nsrdb_path_dict[location[0]]})
-		
-	def simulate_solar(self,ac_cap=7,dc_cap=7):
-		# create system id from input ac capacity and dc capacity
-		sys_id = 'r'+str(ac_cap)+'|'+str(dc_cap)
-		# update system specs in pvwatt
-		self.pvwatt.SystemDesign.assign({
-			'system_capacity':dc_cap,
-			'dc_ac_ratio':dc_cap/ac_cap
-		})
-		# execute solar simulation 
-		self.pvwatt.execute()
-		# read hourly simulation data using pandas
-		hrly_sim = pd.read_csv(
-			self.pvwatt.SolarResource.solar_resource_file,
-			skiprows=2,
-			usecols=['Month','Day','Hour','DHI','DNI','GHI','Temperature']
-		)
-		# get solar output data from pvwatt and assign to 'solar' column of hourly sim data frame
-		hrly_sim['solar'] = self.pvwatt.Outputs.ac
-		# group hourly simulation data by month, day and hour, get mean solar value for each group
-		shapes = hrly_sim.groupby(['Month','Day','Hour'])['solar'].agg('mean').reset_index()
-		# return simulation configuration info along with the calculated shapes
-		return {'spec':sys_id, 'shape':shapes}
+    def set_system_specs(self):
+        """Set system specs"""
+        # assign system information specs to system_design attribute of pvwatt
+        self.pvwatt.SystemDesign.assign(self.system_info['specs'])
+        # assign adjustment factor dict to adjustment_factors attribute  of pvwatt
+        self.pvwatt.AdjustmentFactors.assign({'constant': 0})
 
-	def compare_systems(self):
-		# Capacity Options
-		ac_cap = [5,7,10,12]
-		dc_cap = [5,7,10,12,15]
-		# Create Solar Simulations
-		generaion = []
-		for i in ac_cap:
-			for j in dc_cap:
-				if i/j < 1 and i/j > 2:
-					continue
-				solar_gen = self.simulate_solar(i,j)
-				spec = solar_gen['spec']
-				shape = solar_gen['shape']
-				shape.rename(columns={"solar": "spec"})
-				generaion.append()
-		generaion = reduce(lambda x, y: pd.merge(x, y, on = ['Month','Day','Hour']), generaion)
-		return generaion
+    def fetch_weather_data(self, nrel_api_key, nrel_api_email):
+        """Get Weather Data"""
+        # get latitude and longitude from system info to fetch weather data
+        location = [(self.system_info['location']['lon'],
+                     self.system_info['location']['lat'])]
+        # create instance of FetchResourceFiles using nrel api
+        nrel = tools.FetchResourceFiles(
+            tech='solar',
+            nrel_api_key=nrel_api_key,
+            nrel_api_email=nrel_api_email
+        )
+        # fetch weather data for the given location using nrel api
+        nrel.fetch(location)
+        # get dictionary mapping location to downloaded file path
+        nsrdb_path_dict = nrel.resource_file_paths_dict
+        # assign solar resource file to SolarResource attribute of pvwatt instance
+        self.pvwatt.SolarResource.assign(
+            {'solar_resource_file': nsrdb_path_dict[location[0]]})
+        # fetch resource files based on location
+        nsrdb_path_dict = nrel.fetch(location).resource_file_paths_dict
+        # assign fetched solar resource data path to solar_resource_file attribute of pvwatt
+        self.pvwatt.SolarResource.assign(
+            {'solar_resource_file': nsrdb_path_dict[location[0]]})
+
+    def simulate_solar(self, ac_cap=7, dc_cap=7):
+        """Simulate PV Generation"""
+        # create system id from input ac capacity and dc capacity
+        sys_id = 'r'+str(ac_cap)+'|'+str(dc_cap)
+        # update system specs in pvwatt
+        self.pvwatt.SystemDesign.assign({
+            'system_capacity': dc_cap,
+            'dc_ac_ratio': dc_cap/ac_cap})
+        # execute solar simulation
+        self.pvwatt.execute()
+        # read hourly simulation data using pandas
+        hrly_sim = pd.read_csv(
+            self.pvwatt.SolarResource.solar_resource_file,
+            skiprows=2,
+            usecols=['Month', 'Day', 'Hour', 'DHI', 'DNI', 'GHI', 'Temperature'])
+        # get solar output data from pvwatt and assign to 'solar' column of hourly sim data frame
+        hrly_sim['solar'] = self.pvwatt.Outputs.ac
+        # group hourly simulation data by month, day and hour, get mean solar value for each group
+        shapes = hrly_sim.groupby(['Month', 'Day', 'Hour'])[
+            'solar'].agg('mean').reset_index()
+        # return simulation configuration info along with the calculated shapes
+        return {'spec': sys_id, 'shape': shapes}
